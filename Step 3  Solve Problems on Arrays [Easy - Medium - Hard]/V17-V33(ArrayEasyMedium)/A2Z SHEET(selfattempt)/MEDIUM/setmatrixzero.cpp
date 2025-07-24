22d490599d20d5575ce9900371c3bba2223a812b
RatneshTripathi07 <ratneshgeetatripathi@gmail.com>
2025-07-24 23:55:03 +0530

    setmatrixZero2

---
+++ Step 3  Solve Problems on Arrays [Easy - Medium - Hard]/V17-V33(ArrayEasyMedium)/A2Z SHEET(selfattempt)/MEDIUM/setmatrixzero.cpp
@@ -0,0 +1,14 @@
+// sometimes
+// in matrix ops u gotta check rows and columns and perform elsewhere(other loop)
+
+// revise here..........
+
+#include <bits/stdc++.h>
+using namespace std;
+void setZeroes(vector<vector<int>> &matrix)
+{
+}
+
+int main()
+{
+}
