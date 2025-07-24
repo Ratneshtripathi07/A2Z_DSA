06b5760098be864c3c1891ea3f69b8939273c8d3
RatneshTripathi07 <ratneshgeetatripathi@gmail.com>
2025-07-24 23:55:33 +0530

    90DegRotation

---
+++ Step 3  Solve Problems on Arrays [Easy - Medium - Hard]/V17-V33(ArrayEasyMedium)/A2Z SHEET(selfattempt)/MEDIUM/90DegMatrixRotation.cpp
@@ -0,0 +1,19 @@
+// inplace rotation
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotateMatrix(vector<vector<int>> &matrix)
+{
+    int n = matrix[0].size(); // N*N matrix
+
+    
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+
